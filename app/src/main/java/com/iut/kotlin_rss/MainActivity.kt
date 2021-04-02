54732584e85f831343ae1bf41e3944c9dd3cb265
Sawangg <38500427+Sawangg@users.noreply.github.com>
2021-04-02 14:41:44 +0200

    Ajout Notifications

--- app/src/main/java/com/iut/kotlin_rss/MainActivity.kt
+++ app/src/main/java/com/iut/kotlin_rss/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.iut.kotlin_rss
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.prof.rssparser.Parser
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+    private val channelID = "channel_kotlin_rss";
+    private val notificationId = 101;
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(titre: String, text: String) {
+        val builder = NotificationCompat.Builder(this, channelID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(titre)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build()) 
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        // call sendNotification(titre, text) pour envoyer une notif
     }
 }
