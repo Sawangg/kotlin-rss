612b6f573789295a448652f5021c547c33029924
Gauthier Raphael <raphael.gauthier@etu.unilim.fr>
2021-04-03 16:51:32 +0200

    Delete + Edit flux

--- app/src/main/java/com/iut/kotlin_rss/AddFlux.kt
+++ app/src/main/java/com/iut/kotlin_rss/AddFlux.kt
@@ -37,6 +37,7 @@ class AddFlux : AppCompatActivity() {
                 flux_uri.text.clear()
                 val intent = Intent(this@AddFlux, MainActivity::class.java);
                 startActivity(intent)
+                finish()
             }
         } else {
             Toast.makeText(applicationContext,"Error", Toast.LENGTH_LONG).show()
